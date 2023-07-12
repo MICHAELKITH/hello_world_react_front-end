a3943ea048be5721a6ceb13b49aafe9deeeb75b6
MICHAELKITH <michaelkithinji27@gmail.com>
2023-07-12 17:08:17 +0300

    Hello app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Greeting from './components/Greeting';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/greeting" element={<Greeting />} />
+    </Routes>
+  );
+}
+
+export default App;
